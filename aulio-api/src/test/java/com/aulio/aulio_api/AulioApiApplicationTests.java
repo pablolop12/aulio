9ccbceb086f8e45b58195dc74575d6fad9595f50
Pablo <pablolopezlujan01@gmail.com>
2026-06-08 17:33:28 +0200

    Creación del proyecto y repositorio monorepo 🔥

---
+++ aulio-api/src/test/java/com/aulio/aulio_api/AulioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulio.aulio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
