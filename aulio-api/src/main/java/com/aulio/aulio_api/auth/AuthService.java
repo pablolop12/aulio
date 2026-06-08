4ade45d80ff295c647731ff474990ef48c3ded38
Pablo <pablolopezlujan01@gmail.com>
2026-06-08 23:18:43 +0200

    Creado AuthService

--- aulio-api/src/main/java/com/aulio/aulio_api/auth/AuthService.java
+++ aulio-api/src/main/java/com/aulio/aulio_api/auth/AuthService.java
@@ -4,6 +4,7 @@
 package com.aulio.aulio_api.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.aulio.aulio_api.auth.dto.LoginRequestDTO;
@@ -20,6 +21,35 @@ public class AuthService {
 	//Inyección de dependecias de UserRepository para usar los métodos extendidos en la interfaz
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private JwtService jwtService;
+	
+	//Método de Login, donde se recibe el DTO de la request y se almacena en una variable request.
+	public LoginResponseDTO login (LoginRequestDTO request) {
+		
+		//Busca el usuario por el email y almacena el usuario en la variable user para poder verificar su campo contraseña a continuacion
+		User user = userRepository.findByEmail(request.getEmail())
+			    .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+		
+		//Verificar si la contraseña coincide con la de la base de datos
+		if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) { // La compara con la contraseña codificada
+			throw new RuntimeException("Contraseña no coincidente");
+		}
+		
+		
+		//Preparamos la respuesta, creando una instancia del DTO de respuesta y rellenandolo con los parametros que necesita
+		String token = jwtService.generateToken(user);
+		
+		LoginResponseDTO response = new LoginResponseDTO();
+		response.setToken(token);
+		response.setRole(user.getRole());
+		
+		return response;
+	}
 	
 
 
